5abc54c82bfc9f63cf1cfbda367f64f9f64d469c
weizhuozun <weizhuozun@gmail.com>
2021-09-13 18:36:31 +0800

    多线程

--- hbsoo-game-commons/src/main/java/com/hbsoo/game/inner/InnerMessageDispatcher.java
+++ hbsoo-game-commons/src/main/java/com/hbsoo/game/inner/InnerMessageDispatcher.java
@@ -6,6 +6,10 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Created by zun.wei on 2021/9/9.
@@ -13,7 +17,27 @@ import java.util.concurrent.ConcurrentHashMap;
  */
 public final class InnerMessageDispatcher {
 
+    /**
+     * 处理器缓存
+     */
     private static final Map<Integer, InnerMessageProcessor> cache = new ConcurrentHashMap<>();
+
+    /***
+     * 线程池
+     */
+    static ExecutorService executorService = Executors.newFixedThreadPool(10, new ThreadFactory() {
+        final AtomicInteger atomicInteger = new AtomicInteger();
+
+        @Override
+        public Thread newThread(Runnable runnable) {
+            Thread thread = new Thread(runnable);
+            thread.setName("inner-dispatcher-" + atomicInteger.incrementAndGet());
+            thread.setUncaughtExceptionHandler((thread1, throwable) -> {
+                throwable.printStackTrace();
+            });
+            return thread;
+        }
+    });
 
     /**
      * 消息分发
@@ -25,7 +49,9 @@ public final class InnerMessageDispatcher {
         final String dataJson = message.getDataJson();
         final InnerMessageProcessor innerMessageProcessor = cache.get(msgType);
         if (Objects.nonNull(innerMessageProcessor)) {
-            innerMessageProcessor.process0(batch, dataJson);
+            executorService.execute(() -> {
+                innerMessageProcessor.process0(batch, dataJson);
+            });
             return;
         }
         List<InnerMessageProcessor> processors = GameSpringBeanFactory.getBeansOfTypeWithAnnotation
@@ -35,7 +61,9 @@ public final class InnerMessageDispatcher {
             final int value = innerProcessor.value();
             if (value == msgType) {
                 cache.put(value, processor);
-                processor.process0(batch, dataJson);
+                executorService.execute(() -> {
+                    processor.process0(batch, dataJson);
+                });
             }
         }
 
