575277e755b4da3c9f804471e141f7f06df1aac7
weizhuozun <weizhuozun@gmail.com>
2021-12-27 16:27:45 +0800

    zoo test2

--- hbsoo-utils/src/test/java/com/hbsoo/utils/zoo/ZooTest.java
+++ hbsoo-utils/src/test/java/com/hbsoo/utils/zoo/ZooTest.java
@@ -18,18 +18,24 @@ public class ZooTest {
         //connectionString 	服务器列表，格式host1:port1,host2:port2,...
         String connectionInfo = "192.168.1.215:2181,192.168.1.215:2182,192.168.1.215:2183";
         RetryPolicy retryPolicy = new ExponentialBackoffRetry(1000, 3);
-        CuratorFramework client =
+        CuratorFramework client = CuratorFrameworkFactory.newClient(connectionInfo, retryPolicy);
+
+
+
+        /*CuratorFramework client =
                 CuratorFrameworkFactory.builder()
                         .connectString(connectionInfo)
                         .sessionTimeoutMs(5000)
                         .connectionTimeoutMs(5000)
                         .retryPolicy(retryPolicy)
                         //.namespace("base")
-                        .build();
+                        .build();*/
+
         client.start();
+        client.create().forPath("/my", "myData".getBytes(StandardCharsets.UTF_8));
 
         //client.create().withMode(CreateMode.EPHEMERAL).forPath("/path","aaaa".getBytes(StandardCharsets.UTF_8));
-        client.create().withMode(CreateMode.PERSISTENT).forPath("/path111","aaaa".getBytes(StandardCharsets.UTF_8));
+        //client.create().withMode(CreateMode.PERSISTENT).forPath("/path111","aaaa".getBytes(StandardCharsets.UTF_8));
         //final byte[] bytes = client.getData().forPath("/");
         //System.out.println("bytes = " + bytes);
     }
