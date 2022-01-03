75f290cf3f17275d4b3a05cf624b8d9b758f93fb
zunzhuowei <1808862723@qq.com>
2022-01-03 20:41:33 +0800

    ServerChannelManager.java

--- hbsoo-netty-server/src/main/java/com/hbsoo/server/manager/ServerChannelManager.java
+++ hbsoo-netty-server/src/main/java/com/hbsoo/server/manager/ServerChannelManager.java
@@ -14,7 +14,7 @@ public final class ServerChannelManager {
     /**
      * 全服消息管道
      */
-    //private static final Map<String, Channel> ALL_CHANNEL = new ConcurrentHashMap<>();
+    private static final Map<String, Channel> ALL_CHANNEL = new ConcurrentHashMap<>();
     private static final Map<Long, Channel> USER_ALL_CHANNEL = new ConcurrentHashMap<>();
 
     /**
@@ -22,9 +22,9 @@ public final class ServerChannelManager {
      * @param channels 管道
      */
     public static void add(Channel... channels) {
-        /*for (Channel channel : channels) {
+        for (Channel channel : channels) {
             ALL_CHANNEL.put(channel.id().asLongText(), channel);
-        }*/
+        }
     }
 
     /**
@@ -32,9 +32,9 @@ public final class ServerChannelManager {
      * @param channels 管道
      */
     public static void remove(Channel... channels) {
-        /*for (Channel channel : channels) {
+        for (Channel channel : channels) {
             ALL_CHANNEL.remove(channel.id().asLongText());
-        }*/
+        }
         for (Channel channel : channels) {
             USER_ALL_CHANNEL.entrySet()
                     .removeIf(e ->
@@ -44,13 +44,18 @@ public final class ServerChannelManager {
         }
     }
 
+    public static Channel getChannelByChannelId(String channelId) {
+        return ALL_CHANNEL.get(channelId);
+    }
+
     /**
      * 注册管道保存起来
      * @param userId 用户id
      * @param channel 消息管道
      */
-    public static void regChannel(Long userId, Channel channel) {
+    public static Channel regChannel(Long userId, Channel channel) {
         USER_ALL_CHANNEL.put(userId, channel);
+        return channel;
     }
 
     /**
