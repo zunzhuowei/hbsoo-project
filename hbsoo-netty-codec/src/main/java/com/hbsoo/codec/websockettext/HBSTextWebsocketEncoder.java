77e967d1d4bf1de5d9108b5118effc84e9eb12a9
zunzhuowei <1808862723@qq.com>
2022-01-03 11:21:49 +0800

    修改websocket 消息类型2

--- hbsoo-netty-codec/src/main/java/com/hbsoo/codec/websockettext/HBSTextWebsocketEncoder.java
+++ hbsoo-netty-codec/src/main/java/com/hbsoo/codec/websockettext/HBSTextWebsocketEncoder.java
@@ -1,6 +1,5 @@
 package com.hbsoo.codec.websockettext;
 
-import com.google.protobuf.GeneratedMessageV3;
 import com.hbsoo.msg.model.HBSMessage;
 import com.hbsoo.msg.model.MsgHeader;
 import io.netty.buffer.ByteBuf;
@@ -8,7 +7,6 @@ import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.MessageToMessageEncoder;
-import io.netty.handler.codec.http.websocketx.BinaryWebSocketFrame;
 import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
 
 import java.util.List;
@@ -27,21 +25,30 @@ public class HBSTextWebsocketEncoder extends MessageToMessageEncoder<HBSMessage<
 
             //浏览器 Could not decode a text frame as UTF-8.
 
-            /*final MsgHeader header = msg.getHeader();
+            final MsgHeader header = msg.getHeader();
             final short magicNum = header.getMagicNum();
             final short version = header.getVersion();
             final int msgLen = header.getMsgLen();
-            final int msgType = header.getMsgType();*/
+            final int msgType = header.getMsgType();
 
             /*ByteBuf buffer = Unpooled.buffer(msgLen);
             buffer.writeShort(magicNum)
                     .writeShort(version)
                     .writeInt(msgLen)
                     .writeShort(msgType);*/
-            //.writeBytes();
 
-            ByteBuf buffer = Unpooled.buffer(content.getBytes().length);
-            buffer.writeBytes(content.getBytes());
+            StringBuilder sb = new StringBuilder();
+            sb
+            .append("{")
+            .append("\"").append("magicNum").append("\"").append(":").append(magicNum).append(",")
+            .append("\"").append("version").append("\"").append(":").append(version).append(",")
+            .append("\"").append("msgLen").append("\"").append(":").append(msgLen).append(",")
+            .append("\"").append("msgType").append("\"").append(":").append(msgType).append(",")
+            .append("\"").append("data").append("\"").append(":").append(content)
+            .append("}");
+
+            ByteBuf buffer = Unpooled.buffer(sb.length());
+            buffer.writeBytes(sb.toString().getBytes());
             TextWebSocketFrame textWebSocketFrame = new TextWebSocketFrame(buffer);
             out.add(textWebSocketFrame);
         } else {
--- hbsoo-netty-message/src/main/java/com/hbsoo/msg/model/HBSMessage.java
+++ hbsoo-netty-message/src/main/java/com/hbsoo/msg/model/HBSMessage.java
@@ -22,6 +22,16 @@ public class HBSMessage<T> {
      */
     private T content;
 
+    private Class<T> tClass;
+
+    private Class<T> gettClass() {
+        return tClass;
+    }
+
+    private void settClass(Class<T> tClass) {
+        this.tClass = tClass;
+    }
+
     private void setContent(T content) {
         this.content = content;
     }
@@ -34,6 +44,25 @@ public class HBSMessage<T> {
         final HBSMessage<T> message = new HBSMessage<>();
         MsgHeader header = new MsgHeader();
         message.setHeader(header);
+        message.settClass(contentClazz);
+        return message;
+    }
+
+    public static <T, H extends MsgHeader> HBSMessage<T> create
+            (Class<T> contentClazz, Class<H> hClass) {
+        final HBSMessage<T> message = new HBSMessage<>();
+        MsgHeader header = null;
+        if (hClass == ProtobufMsgHeader.class) {
+            header = new ProtobufMsgHeader();
+        } else if (hClass == StrMsgHeader.class){
+            header = new StrMsgHeader();
+        } else if (hClass == WebsocketProtobufMsgHeader.class) {
+            header = new WebsocketProtobufMsgHeader();
+        } else {
+            throw new RuntimeException("header class [" + hClass.toString() + "] not register");
+        }
+        message.setHeader(header);
+        message.settClass(contentClazz);
         return message;
     }
 
@@ -61,6 +90,11 @@ public class HBSMessage<T> {
         if (content instanceof String) {
             String c = (String) content;
             this.msgLen(c.getBytes().length);
+            this.magicNum(StrMsgHeader.STR_MAGIC_NUM);
+        }
+        if (content instanceof byte[]) {
+            byte[] c = (byte[]) content;
+            this.msgLen(c.length);
         }
         this.content = content;
         return this;
